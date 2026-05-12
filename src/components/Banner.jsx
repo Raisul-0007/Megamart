aeb8b21c7258e3660a447572351d4e94855e55b1
Raisul-0007 <raisulislamriat007@gmail.com>
2026-05-12 19:53:00 +0600

    navber dropdown button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,6 @@ import SliderModule from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ban from "../assets/ban.png";
-import Container from './Container';
 
 const Slider = SliderModule.default;
 
@@ -13,32 +12,32 @@ const Banner = () => {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows:false,
   };
 
+
+
+
   return (
-    <div className="">
-      <Container>
     <div className="slider-container">
       <Slider {...settings}>
         <div>
-          <img className='w-full h-90' src={ban} alt="" />
+          <img className='w-full lg:h-120' src={ban} alt="" />
         </div>
         <div>
-          <img className='w-full h-90' src={ban} alt="" />
+          <img className='w-full lg:h-120' src={ban} alt="" />
         </div>
         <div>
-          <img className='w-full h-90' src={ban} alt="" />
+          <img className='w-full lg:h-120' src={ban} alt="" />
         </div>
         <div>
-          <img className='w-full h-90' src={ban} alt="" />
+          <img className='w-full lg:h-120' src={ban} alt="" />
         </div>
         <div>
-          <img className='w-full h-90' src={ban} alt="" />
+          <img className='w-full lg:h-120' src={ban} alt="" />
         </div>
         
       </Slider>
-    </div>
-    </Container>
     </div>
   )
 }
--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { FaRegUser, FaSearch, FaShoppingCart } from 'react-icons/fa'
 import Container from './Container'
 
 const Navber = () => {
+  let [show, setShow] = useState(false)
+  let navRef = useRef()
+
+  document.addEventListener("click", (e)=>{
+    if(navRef.current.contains(e.target) == true){
+      setShow(!show)
+    }else{
+      setShow(false)
+    }
+  })
   return (
     <div className="border-b border-[#F5F5F5]">
     <Container className='py-5 flex justify-between'>
-      <div className="">
+      <div className="relative w-1/4">
+        <div ref={navRef} className="">
         <h3 className='text-3xl font-bold text-[#1798d0]'>MegaMart</h3>
       </div>
-        <div className="relative">
-          <input className='bg-[#f3f9fb]  rounded-2xl py-3 pl-5 pr-80' type="text" placeholder='Search'/>
-          <div className="absolute text-[#1798d0] top-4 right-5">
+      {show && (
+      <div className="absolute top-17 left-0 z-999 w-40 bg-[#1798d0]">
+        <ul>
+          <li className='py-1 border-b border-[#ffffff33] hover:bg-[#036b979f] cursor-pointer duration-200'><a className='text-white ' href="#">Home</a></li>
+          <li className='py-1 border-b border-[#ffffff33] hover:bg-[#036b979f] cursor-pointer duration-200'><a className='text-white' href="#">Shop</a></li>
+          <li className='py-1 border-b border-[#ffffff33] hover:bg-[#036b979f] cursor-pointer duration-200'><a className='text-white' href="#">About</a></li>
+          <li className='py-1 hover:bg-[#036b979f] cursor-pointer ease-in-out duration-200'><a className='text-white'  href="#">Contact</a></li>
+        </ul>
+      </div>
+      )}
+      </div>
+        <div className="relative w-2/4">
+          <input className='bg-[#f3f9fb]  rounded-2xl py-3 pl-5 w-[80%]' type="text" placeholder='Search'/>
+          <div className="absolute text-[#1798d0] top-4 right-[23%]">
             <FaSearch />
           </div>
           </div>
-          <div className="flex gap-10">
+          <div className="flex gap-10 w-1/4">
             <div className="flex items-center gap-3">
               <div className="text-[#1798d0]"></div>
               <div className="text-[#1798d0]">
