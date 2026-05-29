18901e6c167553d5b82af358e12942a6987e9a92
Raisul-0007 <raisulislamriat007@gmail.com>
2026-05-29 17:40:43 +0600

    29 may 2026

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="flex justify-between">
+      <div className="">
+        footer
+      </div>
+      <div className="">
+        <img src="../assets/hero.png" alt="" />
+      </div>
     </div>
   )
 }
